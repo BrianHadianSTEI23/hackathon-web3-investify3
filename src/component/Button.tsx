f50fbd5f9e05cc6a24541408e3dabdad36a631c7
adndax <adindaputri0517@gmail.com>
2025-03-01 01:07:09 +0700

    fix: button style

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -8,9 +8,9 @@ export const Button: React.FC<ButtonProps> = ({ children, onClick, className }) 
   return (
     <button
       onClick={onClick}
-      className={`bg-[#967BBB] font-semibold text-white text-sm px-5 py-2 rounded-full shadow-md hover:bg-[#8a6ab8] transition-all ${className}`}
+      className={`bg-[#695192] font-semibold text-white text-sm px-5 py-2 rounded-md shadow-md hover:bg-[#775ca6] transition-all ${className}`}
     >
       {children}
     </button>
   );
-};
+}; 
